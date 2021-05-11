22d0866ac7077df7a52e3d84ce348491e5202dfa
Andres <andrex200316@gmail.com>
2021-05-11 10:12:35 -0500

    Mobile Menu V3.0 Stable

--- index.js
+++ index.js
@@ -6,3 +6,29 @@ function openNavigation() {
     nav.className = "top-navigation";
   }
 }
+
+var drops = document.querySelectorAll("li.dropdown");
+for (drop of drops) {
+  drop.addEventListener("click", function manageDropdown(drop) {
+    var drop = window.event || drop; //window.event for IE
+    if (!drop.target) {
+      drop.target = drop.srcElement; //extend target property for IE
+    }
+
+    var parent = drop.target.parentNode;
+
+    while (parent.tagName != "LI") {
+      parent = parent.parentNode;
+    }
+    console.log(parent);
+    if (parent.getAttribute("state") == "open") {
+      parent.setAttribute("state", "close");
+    } else {
+      var otherelements = document.querySelectorAll(".dropdown[state='open']");
+      for (element of otherelements) {
+        element.setAttribute("state", "close");
+      }
+      parent.setAttribute("state", "open");
+    }
+  });
+}
